356b470b53b08570c73844ef6803da0eddd006c2
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-01-07 05:42:48 -0500

    Updates - by Moh

--- ellora/native_patches.py
+++ ellora/native_patches.py
@@ -2,3 +2,7 @@ from frappe.www import printview
 from ellora import native_overrides
 
 printview.get_rendered_template = native_overrides.custom_get_rendered_template
+
+from erpnext.controllers import queries
+
+queries.item_query = native_overrides.custom_item_query
